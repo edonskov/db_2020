a81c25bca7ffe1642650fb474cdd5a1efe72145f
edonskov <radion2002@gmail.com>
2020-09-04 11:26:40 +0300

    donskov/homework3_time

--- src/main/java/date_time/Main.java
+++ src/main/java/date_time/Main.java
@@ -1,9 +1,8 @@
 package date_time;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
@@ -16,33 +15,31 @@ import java.util.Locale;
 public class Main {
 
 
-    public Date convert(LocalDate date) {
-        // todo finish this
-        return null;
+    public static Date convert(LocalDate date) {
+        return Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
     }
 
-    public LocalDateTime convert(Date date) {
-        // todo finish this
-        return null;
+    public static LocalDateTime convert(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 
         // 1981/02/30&18:20
-    public static int daysBetween(String firstDate, String lastDate) {
-        //JSR 310
-        //todo finish this
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy/MM/d");
-        System.out.println();
+    public static int daysBetween(String firstDate, String lastDate) throws ParseException {
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy/MM/dd&hh:mm");
+        LocalDateTime date1 = convert(parser.parse(firstDate));
+        LocalDateTime date2 = convert(parser.parse(lastDate));
+        return (int) Duration.between(date1.toInstant(ZoneOffset.UTC),date2.toInstant(ZoneOffset.UTC)).getSeconds() / 86400;
     }
 
 
-//    public static void main(String[] args) {
-//
-//
-//
+
+    public static void main(String[] args) throws ParseException {
+
+        System.out.println(LocalDate.now());
+        System.out.println(convert(LocalDate.now()));
+        System.out.println(convert(new Date()));
+        System.out.println(daysBetween("1981/01/30&17:20", "1981/02/30&16:20"));
+
 //        LocalTime now1 = LocalTime.now();
 ////        LocalDateTime.of(1932,12,12,12,12,12)
 //        LocalDate now = LocalDate.now();
@@ -54,15 +51,4 @@ public class Main {
 ////        Duration
 //        long between = ChronoUnit.DAYS.between(date, now);
 //        System.out.println("between = " + between);
-//
-////
-////        String str = "java";
-////        str = str.toUpperCase();
-////
-////        Date date = new Date();
-////        System.out.println("date = " + date.getYear());
-//    }
-}
-
-
 
